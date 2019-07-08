bb9df097b27185ee89a3252faf092c5b81aaa975
Liubomyr Kokor <23187063+ljubik@users.noreply.github.com>
2019-07-08 23:09:51 +0300

    add Access-Control-Allow-Origin
    need create .htaccess

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const db = mysql.createConnection({
     host : 'db1.cityhost.com.ua',
